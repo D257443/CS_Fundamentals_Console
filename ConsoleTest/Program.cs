7b1c9c81f820cae551b72feccf2af97f88bc1d19
Damiën van Gageldonk <D257443@edu.rocwb.nl>
2017-09-21 09:33:10 +0200

    Yes, this just didnt work

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,26 @@
+﻿// Using zorgt ervoor dat je al geschreven(voorgebakken) code kan gebruiken. Wat er achter using staat zijn libraries met daaarin de code.
+
+using System;
+// Namespace is een woordenboek, naam van de namespace
+// kan worden aangepast.
+namespace ConsoleTest
+{
+    // Classes = P2
+    class Program
+    {
+        // Static en void ook voor later!
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my first application.");
+
+            int myScore = 5;
+            int yourScore = 8;
+            myScore = myScore + 4;
+            Console.WriteLine("My score is: " + myScore);
+            Console.WriteLine("But your score is: " + yourScore);
+            
+            Console.WriteLine("Press enter key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
